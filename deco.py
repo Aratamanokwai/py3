f16044d5f7765418e641c4d699433de4972db090
ozawa3 <58377611+ozawa3-labo@users.noreply.github.com>
2020-05-17 19:00:33 +0900

    ADD: deco.py

---
+++ deco.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""デコレータの標本.
+
+usage: deco.py [-h] [-V] [-v] [-t]
+
+options:
+    -h, --help      show this help message and exit.
+    -V, --version   shoe version info and exit.
+    -v, --verbose   verbose
+    -t, --test      doctest
+
+The require modules:
+（標準品以外の必要なもの）
+    docopt
+
+履歴情報:
+Ver.0.1   試作
+"""
+
+__version__ = '0.1'
+
+import doctest
+import sys
+import time
+import functools
+import string
+try:
+    from docopt import docopt
+except ModuleNotFoundError:
+    sys.exit(__doc__)
+
+
+def uppercase(func):
+    """Convert returned string to uppercase.
+
+    Args:
+        func(callable)  returnで文字列を返す函數
+    Returns:
+        callable:       returnで大文字の文字列を返す函數
+    Todo:
+        文字列返還函數を引數で與へる。
+
+    >>> uppercase(lambda : 'Hello!')()
+    'HELLO!'
+    >>> uppercase(lambda st: st)('Hi')
+    'HI'
+    """
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        res = func(*args, **kwargs)
+        modi = res.upper()
+        return modi
+    # End of def wrapper(*args, **kwargs):
+
+    return wrapper
+# End of def uppercase(func):
+
+
+def cnvstr(func0, func1):
+    """Convert returned string to cnvstr.
+
+    除蟲中、想定通りに動きません。
+
+    Args:
+        func(callable)  returnで文字列を返す函數
+    Returns:
+        callable:       returnで大文字の文字列を返す函數
+    Todo:
+        文字列返還函數を引數で與へる。
+
+    >>> cnvstr(string.ascii_uppercase, lambda st: st)('Hi')
+    'HI'
+    """
+    @functools.wraps(func1)
+    def wrapper(*args, **kwargs):
+        res = func1(*args, **kwargs)
+        modi = func0(res)
+        return modi
+    # End of def wrapper(*args, **kwargs):
+
+    return wrapper
+# End of def cnvstr(func0, func1):
+
+
+def trace(func):
+    """Trace the function.
+
+    Args:
+        func(callable)  函數
+    Returns:
+        callable:       追跡情報を付加した函數
+    Todo:
+        Nestによるindent.
+
+    >>> trace(lambda st: st)('Hi')
+    >TRACE: <lambda>() with ('Hi',), {}
+    >  RET: <lambda>() returned 'Hi'
+    'Hi'
+    """
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f'>TRACE: {func.__name__}()'
+              f' with {args}, {kwargs}')
+        res = func(*args, **kwargs)
+        print(f'>  RET: {func.__name__}()'
+              f' returned {res!r}')
+        return res
+    # End of def wrapper():
+
+    return wrapper
+# End of def trace(func):
+
+
+def stopwatch(func):
+    """Stopwatch.
+
+    Args:
+        func(callable)  函數
+    Returns:
+        callable:       處理時間表示を付加した函數
+    """
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        res = func(*args, **kwargs)
+        stop = time.time()
+        print(f"處理時間： {(stop-start):.5f} 秒")
+        return res
+    # End of def wrapper():
+
+    return wrapper
+# End of def stopwatch(func):
+
+
+@stopwatch
+@trace
+def main():
+    """Do main function.
+
+    >>> import deco
+    """
+    args = docopt(__doc__)
+
+    if args['--version']:
+        print(f'Ver: {__version__}')
+        sys.exit()
+
+    if args['--test']:
+        doctest.testmod(verbose=args['--verbose'])
+        sys.exit()
+
+    if args['--verbose']:
+        print('[*] deco.py')
+# End of def main():
+
+
+if __name__ == '__main__':
+    main()
--- temp.py
+++ temp.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 """算譜の説明.
 
-usage: temp.py [-h] [-v] [-t] [--sum] N [N...]
+usage: temp.py [-h] [-V] [-v] [-t] [--sum] N [N...]
 
 options:
-    -h, --help      show this help message and exit
+    -h, --help      show this help message and exit.
+    -V, --version   show version info and exit.
     -v, --verbose   verbose
     -t, --test      doctest
     --sum           sum the integers (default: find the max)
@@ -13,31 +14,71 @@ options:
 The require modules:
 （標準品以外の必要なもの）
     docopt
+
+履歴情報:
+Ver.0.0   雛型
+Ver.0.1   試作
 """
+
+__version__ = '0.0'
 
 import doctest
 import sys
-from docopt import docopt
+try:
+    from docopt import docopt
+except ModuleNotFoundError:
+    sys.exit(__doc__)
 
-EX_OK = 0
+
+class Car(object):
+    """車輛.
+
+    Attributes:
+        color (str):    色
+
+    >>> Car()
+    class Car.
+    """
+
+    def __init__(self, color='red'):
+        """Iniitialize Car."""
+        self.color = color
+    # End of def __init__(self, color, mileage):
+
+    def __repr__(self):
+        """Shoe representation.
+
+        >>> car = Car()
+        >>> repr(car)
+        'class Car.'
+        """
+        return f'class {__class__.__name__}.'
+    # End of def __repr__(self):
+# End of class Car(object):
 
 
 def main():
-    """Do main function."""
-    vbs = False
+    """Do main function.
+
+    >>> import temp
+    """
     args = docopt(__doc__)
-    # print (args)
-    if args['--verbose']:
-        vbs = True
     if args['--test']:
-        doctest.testmod(verbose=vbs)
-        sys.exit(EX_OK)
-    _ns = map(int, args["N"])
+        doctest.testmod(verbose=args['--verbose'])
+        sys.exit()
+
+    if args['--version']:
+        print('Ver: {}'.format(__version__))
+        sys.exit()
+
+    if args['--verbose']:
+        print('[*] temp.py')
+
+    nums = map(int, args["N"])
     if args["--sum"]:
-        print(sum(_ns))
+        print(sum(nums))
     else:
-        print(max(_ns))
-    # print(f'vbs: {vbs}')
+        print(max(nums))
 # End of def main():
 
 
