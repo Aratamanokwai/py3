0d7680d285155b4b7b0633b98d07fa4082253ad3
ozawa3 <58377611+ozawa3-labo@users.noreply.github.com>
2020-11-03 17:26:11 +0900

    CHG: today.py: 1.2

--- scrape/today.py
+++ scrape/today.py
@@ -1,97 +1,75 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 # 履歴情報:
 # Ver.0.0   雛型
 # Ver.0.1   試作
+# Ver.0.2   一覽化
 
-__prog__ = 'tody.py'
+__prog__ = 'today.py'
 __description__ = '今日は何の日？'
-__epilog__ = 'Python 3.7 以上で動作します。'
-__version__ = '0.1'
+__epilog__ = 'Python 3.9 以上で動作します。'
+__version__ = '0.2'
 
 import sys
 import argparse
 import doctest
 import requests
 from bs4 import BeautifulSoup
+import pprint as pp
 
 
-url = 'https://ja.wikipedia.org'
+g_url = 'https://ja.wikipedia.org'
 
 
-class Car:
-    """車輛.
-
-    Attributes:
-        color (str):    色
-
-    >>> Car()
-    Car(red)
-    """
-
-    def __init__(self, color='red'):
-        """Iniitialize Car."""
-        self.color = color
-    # End of def __init__(self, color, mileage):
-
-    def __repr__(self):
-        """Show the representation.
-
-        >>> car = Car()
-        >>> repr(car)
-        'Car(red)'
-        """
-        return f'{self.__class__.__name__}({self.color})'
-    # End of def __repr__(self):
-
-    def __str__(self):
-        """Show the string.
-
-        >>> car = Car()
-        >>> str(car)
-        'class Car'
-        """
-        return f'class {self.__class__.__name__}'
-    # End of def __str__(self):
-# End of class Car:
-
-
-def run(vbs=False):
+def get_info(url, vbs=False):
     """處理實行.
 
     Args:
+        url(str):       URL
         vbs(bool):      詳細情報表示旌旗
     Returns:
-        True:           處理成功
-        False:          處理失敗
+        list;           取得情報
     Raises:
         TypeError:      引數の型の不具合
         ValueError:     引數の値の不具合
         AssertionError: 不具合
-    Examples:
-        >>> run(1)
+    Tests:
+        >>> get_info(1)
+        Traceback (most recent call last):
+            ...
+        AssertionError: [!!] <url> must be a string.
+        >>> get_info('url', 1)
         Traceback (most recent call last):
             ...
         AssertionError: [!!] <vbs> must be boolean.
     """
+    assert isinstance(url, str), '[!!] <url> must be a string.'
     assert isinstance(vbs, bool), '[!!] <vbs> must be boolean.'
 
     if vbs:
-        print('[*] Run:')
+        print('[*] get_info():')
 
     res = requests.get(url)
     soup = BeautifulSoup(res.content, 'html.parser')
-    today = soup.find('div', attrs={'id': 'on_this_day'}).text
+    today = soup.find('div', attrs={'id': 'on_this_day'})
+    #today = soup.find('div', attrs={'id': 'on_this_day'}).text
 
-    print(today)
-# End of def run(vbs=False):
+    entries = today.find_all('li')
+    today_list = []
+
+    for idx, entry in enumerate(entries):
+        today_list.append([idx + 1, entry.get_text()])
+
+    return today_list
+# End of def get_info(url, vbs=False):
 
 
 def main():
     """主函數.
 
-    >>> import tody
+    Tests:
+        >>> import today
     """
     parser = argparse.ArgumentParser(
         prog=__prog__,
@@ -121,7 +99,8 @@ def main():
     if args.verbose:
         print(f'Program: {__prog__}')
 
-    run(args.verbose)
+    res = get_info(g_url, args.verbose)
+    pp.pprint(res)
 # End of def main():
 
 
